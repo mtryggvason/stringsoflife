7b41e3b2fd62d3886115280403b0620b22f443cb
mtryggvason <m.tryggvason@kiska.com>
2019-09-20 19:26:02 +0200

    wait for audio to load

--- scripts/main.js
+++ scripts/main.js
@@ -14,14 +14,13 @@
 		], function(App,BufferLoader){
 			'use strict';
 			var samples = [];
-			for(var i=1;i<22;i++){
+			for(var i = 1; i< 16; i++) {
 				samples[i-1] = 'cello/'+i+'.mp3';
 			}
 			var app = new App();
 			window.AudioContext = window.AudioContext || window.webkitAudioContext;
 			var context = new AudioContext();
-			var bufferLoader = new BufferLoader(context,samples, () => {});
-			app.initialize();
+			var bufferLoader = new BufferLoader(context,samples,app.initialize);
 			app.setBufferLoader(bufferLoader);
 			bufferLoader.load();
 		});
